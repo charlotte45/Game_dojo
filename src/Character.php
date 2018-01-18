3f244630a5080f70d7bfe8a20688c831de86abea
charlotte45 <charlotte.prieur@yahoo.fr>
2018-01-18 10:10:38 +0100

    first commit

---
+++ src/Character.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 18/01/18
+ * Time: 10:08
+ */
+
+namespace game;
+
+
+class Character
+{
+
+}
---
+++ src/Map.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 18/01/18
+ * Time: 10:09
+ */
+
+namespace game;
+
+
+class Map
+{
+
+}
